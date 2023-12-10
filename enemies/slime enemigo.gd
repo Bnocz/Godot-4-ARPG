b87397dad57c9ec0ebae7d0379b58244a372e8b6
Bertram Nocon <BertNocon2@gmail.com>
2023-12-09 22:09:06 -0800

    added enemy framework

---
+++ enemies/slime enemigo.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed = 40
+@export var limit = 0.5
+@onready var animations = $AnimatedSprite2D
+var startPosition
+var endPosition
+
+func _ready():
+	startPosition = position
+	endPosition = startPosition + Vector2(0, 3*16)
+
+func changeDirection():
+	var tempEnd = endPosition
+	endPosition = startPosition
+	startPosition = tempEnd
+
+func updateVelocity():
+	var moveDirection = endPosition - position
+	if moveDirection.length() < limit:
+		changeDirection()
+	velocity = moveDirection.normalized()*speed
+
+func updateAnimation():
+	var animationString = "walkUp"
+	if velocity.y > 0:
+		animationString = "walkDown"
+	
+	animations.play(animationString)
+
+func _physics_process(delta):
+	updateVelocity()
+	move_and_slide()
+	updateAnimation()
